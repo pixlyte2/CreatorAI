007baf46ff6e6297d463537b96790bc72c6e04c7
pixlyte2 <pixlyt.e2@gmail.com>
2026-02-04 18:06:37 +0530

    codes

--- server/controllers/promptController.js
+++ server/controllers/promptController.js
@@ -1,62 +1,84 @@
-const Prompt = require("../models/prompt");
-const PromptType = require("../models/promptType");
-const Channel = require("../models/channel");
+
+
+// const Prompt = require("../models/Prompt");
+
+// const createPrompt = async (req, res) => {
+//   const prompt = await Prompt.create({
+//     ...req.body,
+//     companyId: req.user.companyId,
+//     createdBy: req.user.id
+//   });
+//   res.json(prompt);
+// };
+
+// const getPrompts = async (req, res) => {
+//   const prompts = await Prompt.find({ companyId: req.user.companyId });
+//   res.json(prompts);
+// };
+
+// const updatePrompt = async (req, res) => {
+//   const prompt = await Prompt.findOneAndUpdate(
+//     { _id: req.params.id, companyId: req.user.companyId },
+//     req.body,
+//     { new: true }
+//   );
+//   res.json(prompt);
+// };
+
+// const deletePrompt = async (req, res) => {
+//   await Prompt.findOneAndDelete({
+//     _id: req.params.id,
+//     companyId: req.user.companyId
+//   });
+//   res.json({ message: "Prompt deleted" });
+// };
+
+// const getPromptTypesByChannel = async (req, res) => {
+//   const types = await Prompt.distinct("promptType", {
+//     channelName: req.params.channelName,
+//     companyId: req.user.companyId
+//   });
+//   res.json(types);
+// };
+
+// module.exports = {
+//   createPrompt,
+//   getPrompts,
+//   updatePrompt,
+//   deletePrompt,
+//   getPromptTypesByChannel
+// };
+
+
+
+
+
+
+const Prompt = require("../models/Prompt");
 
 const createPrompt = async (req, res) => {
-  const { channelId, promptTypeId, promptText, aiModel } = req.body;
-
-  // 🔒 validate channel
-  const channel = await Channel.findOne({
-    _id: channelId,
-    companyId: req.user.companyId
-  });
-  if (!channel) {
-    return res.status(400).json({ message: "Invalid channel" });
-  }
-
-  // 🔒 validate prompt type
-  const type = await PromptType.findOne({
-    _id: promptTypeId,
-    channelId,
-    companyId: req.user.companyId
-  });
-  if (!type) {
-    return res.status(400).json({ message: "Invalid prompt type" });
-  }
-
   const prompt = await Prompt.create({
-    channelId,
-    promptTypeId,
-    promptText,
-    aiModel,
+    ...req.body,
     companyId: req.user.companyId,
     createdBy: req.user.id
   });
-
   res.json(prompt);
 };
 
 const getPrompts = async (req, res) => {
   const prompts = await Prompt.find({
     companyId: req.user.companyId
-  })
-    .populate("channelId", "name")
-    .populate("promptTypeId", "name");
-
+  });
   res.json(prompts);
 };
 
 const updatePrompt = async (req, res) => {
-  const updated = await Prompt.findOneAndUpdate(
-    {
-      _id: req.params.id,
-      companyId: req.user.companyId
-    },
+  const prompt = await Prompt.findOneAndUpdate(
+    { _id: req.params.id, companyId: req.user.companyId },
     req.body,
     { new: true }
   );
-
-  res.json(updated);
+  res.json(prompt);
 };
 
 const deletePrompt = async (req, res) => {
@@ -64,13 +86,21 @@ const deletePrompt = async (req, res) => {
     _id: req.params.id,
     companyId: req.user.companyId
   });
-
   res.json({ message: "Prompt deleted" });
+};
+
+const getPromptTypesByChannel = async (req, res) => {
+  const types = await Prompt.distinct("promptType", {
+    channelName: req.params.channelName,
+    companyId: req.user.companyId
+  });
+  res.json(types);
 };
 
 module.exports = {
   createPrompt,
   getPrompts,
   updatePrompt,
-  deletePrompt
+  deletePrompt,
+  getPromptTypesByChannel
 };
